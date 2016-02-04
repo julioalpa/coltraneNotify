7b981d19fd03f8923acf1fdb6674afa7ae126bcd
julio_alpa <julitosanchez1@gmail.com>
2016-02-04 17:40:44 -0300

    gulp file

--- coltrane/gulpfile.js
+++ coltrane/gulpfile.js
@@ -12,5 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    //mix.sass('app.scss');
+    mix.scripts([
+        '../../../bower_components/jquery/dist/jquery.min.js'
+    ]);
 });
